8df3c0eac03b7cdf35e915dcfe5ae5dfbf262edd
Steve Wall <swall@ecsteam.com>
2017-09-05 10:49:32 -0600

    wip

---
+++ ci/tasks/create-bosh-test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+set -x
+
+EXPECTED_HELP="USAGE:
+   create-bosh.sh <IaaS>
+
+Support IaaSes - vsphere, gcp, azure, aws"
+
+ACTUAL_HELP=./create-bosh.sh help
+
+# Fail test if help message is not as expected.
+if [[ $ACTUAL_HELP != $EXPECTED_HELP ]]; then
+  echo "Unexpected help message : $ACTUAL_HELP"
+  exit 1
+fi
---
+++ create-bosh.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -e
+###############################################################
+#
+# Create a BOSH environment based on the given parameters.
+#
+###############################################################
+
+set -x
+
+function usage() {
+cat <<EOF
+USAGE:
+   create-bosh.sh <IaaS>
+
+Support IaaSes - vsphere, gcp, azure, aws
+EOF
+}
+
+####################################
+#
+#   MAIN
+#
+####################################
+BD=~/workspace/bosh-deployment
+mkdir -p $BD
+
+
+if [ -z $command ] || [ $command == "help" ]; then
+	usage
+	exit 1
+fi
+
+git clone git@github.com:cloudfoundry/bosh-deployment.git
+
+# bosh2 create-env $BD/bosh.yml \
+#   --state=bosh-init-state.json \
+#   --vars-store=./creds.yml \
+#   -o $BD/vsphere/cpi.yml \
+#   -o $BD/misc/dns.yml \
+#   -v director_name=steve-wall-cgi-class \
+#   -v internal_cidr=172.28.98.0/24 \
+#   -v internal_gw=172.28.98.1 \
+#   -v internal_ip=172.28.98.50 \
+#   -v network_name="Lab09-NetH" \
+#   -v vcenter_dc=Lab09-Datacenter01 \
+#   -v vcenter_ds=nfs-lab09-vol1 \
+#   -v vcenter_ip=172.29.0.11 \
+#   -v internal_dns=[172.29.0.5] \
+#   -v vcenter_user="lab09admin@lab.ecsteam.local" \
+#   -v vcenter_password="Ecsl@b99" \
+#   -v vcenter_templates=steve-wall-bosh-templates \
+#   -v vcenter_vms=steve-wall-bosh-vms \
+#   -v vcenter_disks=steve-wall-bosh-disks \
+#   -v vcenter_cluster=Lab09-Cluster01
+#
+# bosh2 -e 172.28.98.50 --ca-cert <(bosh2 int ./creds.yml --path /director_ssl/ca) alias-env lab09
