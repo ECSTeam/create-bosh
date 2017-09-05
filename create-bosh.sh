de283c9e33e1442336c04dce4bbb427d8af4d7bc
kamattson <kmattson@ecsteam.com>
2017-09-05 14:16:12 -0600

    updates

--- create-bosh.sh
+++ create-bosh.sh
@@ -21,8 +21,7 @@ EOF
 #   MAIN
 #
 ####################################
-BD=~/workspace/bosh-deployment
-mkdir -p $BD
+BD=bosh-deployment
 
 command=$1
 
@@ -39,25 +38,25 @@ fi
 
 git clone git@github.com:cloudfoundry/bosh-deployment.git
 
-# bosh2 create-env $BD/bosh.yml \
-#   --state=bosh-init-state.json \
-#   --vars-store=./creds.yml \
-#   -o $BD/vsphere/cpi.yml \
-#   -o $BD/misc/dns.yml \
-#   -v director_name=steve-wall-cgi-class \
-#   -v internal_cidr=172.28.98.0/24 \
-#   -v internal_gw=172.28.98.1 \
-#   -v internal_ip=172.28.98.50 \
-#   -v network_name="Lab09-NetH" \
-#   -v vcenter_dc=Lab09-Datacenter01 \
-#   -v vcenter_ds=nfs-lab09-vol1 \
-#   -v vcenter_ip=172.29.0.11 \
-#   -v internal_dns=[172.29.0.5] \
-#   -v vcenter_user="lab09admin@lab.ecsteam.local" \
-#   -v vcenter_password="Ecsl@b99" \
-#   -v vcenter_templates=steve-wall-bosh-templates \
-#   -v vcenter_vms=steve-wall-bosh-vms \
-#   -v vcenter_disks=steve-wall-bosh-disks \
-#   -v vcenter_cluster=Lab09-Cluster01
-#
-# bosh2 -e 172.28.98.50 --ca-cert <(bosh2 int ./creds.yml --path /director_ssl/ca) alias-env lab09
+bosh2 create-env $BD/bosh.yml \
+  --state=bosh-init-state.json \
+  --vars-store=./creds.yml \
+  -o $BD/vsphere/cpi.yml \
+  -o $BD/misc/dns.yml \
+  -v director_name=steve-wall-cgi-class \
+  -v internal_cidr=172.28.98.0/24 \
+  -v internal_gw=172.28.98.1 \
+  -v internal_ip=172.28.98.50 \
+  -v network_name="Lab09-NetH" \
+  -v vcenter_dc=Lab09-Datacenter01 \
+  -v vcenter_ds=nfs-lab09-vol1 \
+  -v vcenter_ip=172.29.0.11 \
+  -v internal_dns=[172.29.0.5] \
+  -v vcenter_user="lab09admin@lab.ecsteam.local" \
+  -v vcenter_password="Ecsl@b99" \
+  -v vcenter_templates=steve-wall-bosh-templates \
+  -v vcenter_vms=steve-wall-bosh-vms \
+  -v vcenter_disks=steve-wall-bosh-disks \
+  -v vcenter_cluster=Lab09-Cluster01
+
+bosh2 -e 172.28.98.50 --ca-cert <(bosh2 int ./creds.yml --path /director_ssl/ca) alias-env lab09
