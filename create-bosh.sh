6f22576fd62f03c370bd6536b9fd8132e99b00e5
kamattson <kmattson@ecsteam.com>
2017-09-05 14:32:46 -0600

    updates

--- create-bosh.sh
+++ create-bosh.sh
@@ -35,8 +35,7 @@ if [ $command == "help" ]; then
   exit 0
 fi
 
-git config http.sslVerify “false”
-git clone git@github.com:cloudfoundry/bosh-deployment.git
+git -c http.sslVerify=false clone git@github.com:cloudfoundry/bosh-deployment.git
 
 if [ $command == "vsphere" ]; then
   bosh2 create-env $BD/bosh.yml \
