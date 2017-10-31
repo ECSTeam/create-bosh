d8b4ac01a1b5283f94a364775f20b9fc192affd9
Steve Wall <swall@ecsteam.com>
2017-10-31 15:03:17 -0600

    trimmed set-pipeline to be specific.

--- ci/set-pipeline.sh
+++ ci/set-pipeline.sh
@@ -2,31 +2,11 @@
 
 set -ex
 
-if [ "$1" = "" ] || [ "$2" = "" ] || [ "$3" = "" ] || [ "$4" = "" ]; then
-  echo $0: usage: $0 concourse-target pipeline.yml config.yml pipeline-name
-  exit
-fi
+concourse_target=gold
+pipeline=pipeline.yml
+pipeline_name=create-bosh-iac-test
 
-concourse_target=$1
-pipeline=$2
-config=$3
-pipeline_name=$4
-
-if [ ! -e  $pipeline ]; then
-  echo $0: Pipeline file does not exist: $pipeline
-  exit
-fi
-
-if [ ! -e  $config ]; then
-  echo $0: Config file does not exist: $config
-  exit
-fi
-
-cat $config >> temp-config.yml
-
-fly -t $concourse_target set-pipeline -c $pipeline -l temp-config.yml -p $pipeline_name -n
-
-rm temp-config.yml
+fly -t $concourse_target set-pipeline -c $pipeline -p $pipeline_name -n
 
 fly -t $concourse_target unpause-pipeline -p $pipeline_name
 
